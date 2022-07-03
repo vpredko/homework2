82b0e35cf94873cec1e6dcfbbddd636b67fecc84
Vika <vpredko@mail.ru>
2022-07-03 16:28:43 +0300

    Solve task2

---
+++ task/task2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine ("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string number2 = Convert.ToString(number);
+if  (number <= 99)
+    Console.WriteLine("третьей цифры нет");
+else
+    Console.WriteLine("третья цифра -> " + number2[2]);
