a5bad2fe5cf54f5f2372ce8e311c38ad7a1b303e
Vika <vpredko@mail.ru>
2022-07-03 16:47:18 +0300

    Solve task3

---
+++ task/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите цифру: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+if (number <= 5 & number >= 1)
+    Console.WriteLine ("нет");
+else if (number == 6 || number == 7)
+    Console.WriteLine("да");
+else
+    Console.WriteLine ("нет такого дня недели");
+    
+
+
